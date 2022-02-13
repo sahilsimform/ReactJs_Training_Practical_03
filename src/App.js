0c2716cf38bea20f0d373fe35ac6e5a1c735b09f
sahilsimform <sahilsinghsimform@gmail.com>
2022-02-13 15:21:42 +0530

    Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Todo from "./components/Todo";
+// import Todo from "./components/testTodo";
+import Time from "./components/Time";
+
+function App() {
+  return (
+    <div className="container ">
+      <>
+        <h1 className="neonText ">Welcome To Sahil's To-do App</h1>
+        <Time />
+        <Todo />
+      </>
+    </div>
+  );
+}
+
+export default App;
