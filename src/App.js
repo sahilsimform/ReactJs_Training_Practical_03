2578e6bb70d6ab4e4abe280d97ddd79c4721468d
sahilsimform <sahilsinghsimform@gmail.com>
2022-02-14 08:56:33 +0530

    Updating App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import Time from "./components/Time";
 
 function App() {
   return (
-    <div className="container ">
-      <>
+    <>
+      <div className="container ">
         <h1 className="neonText ">Welcome To Sahil's To-do App</h1>
         <Time />
         <Todo />
-      </>
-    </div>
+      </div>
+    </>
   );
 }
 
